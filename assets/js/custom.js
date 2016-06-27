61ff5d15622c12e9822d07ac3945d99346465b0b
Bhawna Thakur <bhawna.thakur@venturepact.com>
2016-06-27 15:55:19 +0530

    Changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -76,7 +76,10 @@ $(document).mouseup(function(e) {
     if (!container.is(e.target) // if the target of the click isn't the container...
         && container.has(e.target).length === 0) // ... nor a descendant of the container
     {
-        container.hide();
+        container.hide('slide', {
+            direction: 'right',
+            easing: 'linear'
+        }, 400);
     }
 });
 /* End of funtion */
