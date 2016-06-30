7857c02c2374516f24bdff099da4e149dc79844f
Bhawna Thakur <bhawna.thakur@venturepact.com>
2016-06-30 16:50:38 +0530

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -131,11 +131,11 @@ $(document).on('click', '.ed-sidebar a.section-head ', function() {
     $('a.head span.active-circle').removeClass('active-circle');
     $(this).find("span").addClass('active');
 })
-$('.type-details .show-check').click(function() {
-    if (this.checked)
-        $('.type-details .div-check').fadeIn('slow');
-    else
-        $('.type-details .div-check').fadeOut('slow');
+$('.show-check').click(function() {
+    if ($(this).prop('checked')) {
+        $(this).parents('.type-details').find('.div-check').fadeIn('slow');
+    } else
+        $(this).parents('.type-details').find('.div-check').fadeOut('slow');
 });
 $(document).on('click', '.editor-tab', function() {
     var toggleValue = $(this).data("toggle");
